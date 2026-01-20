b4e43d3610caeebfa3e6fbed2bce0c49783ad9da
Sandeep Kumar Jaiswal~ <jaiswalsandeep508@gmail.com>
2026-01-20 08:39:11 +0530

    I have created Model and Repository packages for category module

---
+++ src/main/java/com/ecommerce/model/Category.java
@@ -0,0 +1,50 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "categories",
+        uniqueConstraints ={
+                @UniqueConstraint(columnNames = "name")
+        }
+)
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false,  unique = true)
+    private String name;
+
+    @Column(length = 200)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
